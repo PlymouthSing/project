a3527c89287c302c208e777ecb2b49df6b647a53
PlymouthSing <singwc@gmail.com>
2020-09-01 21:49:51 +0800

    Debuging

--- index.py
+++ index.py
@@ -5,6 +5,37 @@ import cgitb
 import datetime
 import hashlib
 import pymongo
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords, wordnet
+from nltk import word_tokenize, pos_tag
+from nltk.stem import WordNetLemmatizer
+from pickle import dump, load
+from bson.objectid import ObjectId
+
+class customTokenizer:
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    
+    def __call__(self, d):
+        tokenized_word = [t for t in word_tokenize(d) if t.isalnum()]
+        
+        stop_words = set(stopwords.words("english"))
+        filtered = []
+        for w in tokenized_word:
+            if w not in stop_words:
+                filtered.append(w)
+        
+        #tag = pos_tag([f])[0][1][0].upper()
+        tag_dict = {
+                "J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV
+        }
+        
+        return [self.wnl.lemmatize(f,\
+                tag_dict.get(pos_tag([f])[0][1][0].upper(), wordnet.NOUN))\
+                for f in filtered]
 
 class Block:
     def __init__(self, index, data, timestamp, p_hash):
@@ -13,7 +44,7 @@ class Block:
         self.timestamp = timestamp
         self.p_hash = p_hash
         self.hash = self.hashing()
-
+    
     def hashing(self):
         h = hashlib.sha256()
         h.update(str(self.index).encode("UTF-8"))
@@ -25,19 +56,33 @@ class Block:
 class Chain:
     def __init__(self):
         self.blocks = [self.initial_block()]
-
+    
     def chain_size(self):
         return len(self.blocks) - 1
-
+    
     def add_block(self, data):
         self.blocks.append(Block(len(self.blocks),
             data,
             datetime.datetime.utcnow(),
             self.blocks[len(self.blocks) - 1].hash))
-
+    
     def initial_block(self):
         return Block(0, "Blockchain starts here!",
             datetime.datetime.utcnow(), "Hash Code!")
+
+class vChain(Chain):
+    def __init__(self, ini_timestamp):
+        self.blocks = [self.initial_block(ini_timestamp)]
+    
+    def add_block(self, data, timestamp):
+        self.blocks.append(Block(len(self.blocks),
+            data,
+            timestamp,
+            self.blocks[len(self.blocks) - 1].hash))
+    
+    def initial_block(self, ini_timestamp):
+        return Block(0, "Blockchain starts here!",
+            ini_timestamp, "Hash Code!")
 
 cgitb.enable()
 form = cgi.FieldStorage()
@@ -53,6 +98,8 @@ result = None
 gen = None
 hashcode = None
 find = None
+filled = 0
+count = 0
 
 if form.getvalue("companyname"):
     companyname = form.getvalue("companyname")
@@ -92,10 +139,80 @@ if companyname\
         and editor\
         and typeofpolicy:
     filled = 1
-else:
-    filled = 0
 
 if gen == "Generate" and filled == 1:
+    result = ""
+    txt = open("policy_bank.txt")
+    line = txt.read().splitlines()
+    txt.close()
+    rule = [x for x in line if x]
+    for i, x in enumerate(rule):
+        if x[len(x) - 1] != ".":
+            rule[i] += "."
+    
+    if typeofpolicy == "WiFi":
+        pkl = load(open("wifi.pkl", "rb"))
+        vectorize = pkl.transform(rule)
+        
+        score = []
+        for i, j in enumerate(vectorize.toarray()):
+            s = 0
+            for k, l in enumerate(pkl.get_feature_names()):
+                if l == "wifi" and j[k]:
+                    j[k] += 100
+                if businesstype == "Education":
+                    if l == "School" and j[k]:
+                        j[k] += 20
+                    if l == "student" and j[k]:
+                        j[k] += 20
+                    if l == "company" and j[k]:
+                        j[k] -= 20
+                if businesstype == "IT Services":
+                    if l == "School" and j[k]:
+                        j[k] -= 20
+                    if l == "student" and j[k]:
+                        j[k] -= 20
+                    if l == "company" and j[k]:
+                        j[k] += 20
+                s += j[k]
+            score += ((rule[i], s),)
+        score.sort(key = lambda v: v[1], reverse = True)
+        
+        for m in score:
+            if m[1] > 100:
+                result += m[0]
+    elif typeofpolicy == "Password":
+        pkl = load(open("password.pkl", "rb"))
+        vectorize = pkl.transform(rule)
+        
+        score = []
+        for i, j in enumerate(vectorize.toarray()):
+            s = 0
+            for k, l in enumerate(pkl.get_feature_names()):
+                if l == "password" and j[k]:
+                    j[k] += 100
+                if businesstype == "Education":
+                    if l == "School" and j[k]:
+                        j[k] += 20
+                    if l == "student" and j[k]:
+                        j[k] += 20
+                    if l == "company" and j[k]:
+                        j[k] -= 20
+                if businesstype == "IT Services":
+                    if l == "School" and j[k]:
+                        j[k] -= 20
+                    if l == "student" and j[k]:
+                        j[k] -= 20
+                    if l == "company" and j[k]:
+                        j[k] += 20
+                s += j[k]
+            score += ((rule[i], s),)
+        score.sort(key = lambda v: v[1], reverse = True)
+        
+        for m in score:
+            if m[1] > 100:
+                result += m[0]
+    
     document = Chain()
     document.add_block(companyname)
     document.add_block(businesstype)
@@ -104,14 +221,68 @@ if gen == "Generate" and filled == 1:
     document.add_block(numberofservers)
     document.add_block(editor)
     document.add_block(typeofpolicy)
-    result = "empty"
     document.add_block(result)
+    document.add_block("Blockchain ends here!")
     
-    hashcode = document.blocks[document.chain_size()].hashing()
+    hashcode = document.blocks[document.chain_size()].p_hash
+    
+    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    database = client["database"]
+    collection = database["collection"]
+    
+    for i in range(document.chain_size() + 1):
+        info = {
+            "index": str(document.blocks[i].index),
+            "data": str(document.blocks[i].data),
+            "timestamp": str(document.blocks[i].timestamp),
+            "p_hash": str(document.blocks[i].p_hash)
+            }
+        collection.insert_one(info)
+elif find == "Find" and hashcode != None:
+    result = ""
+    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    database = client["database"]
+    collection = database["collection"]
+    
+    query = {"p_hash": hashcode}
+    target = collection.find_one(query)
+    
+    h = int(str(target["_id"]), 16) - 9
+    query = {"_id": ObjectId(format(h, "x"))}
+    target = collection.find_one(query)
+    
+    if target:
+        vdoc = vChain(target["timestamp"])
+    
+    while target and vdoc.blocks[count].p_hash == target["p_hash"]:
+        h += 1
+        query = {"_id": ObjectId(format(h, "x"))}
+        target = collection.find_one(query)
+        if target:
+            vdoc.add_block(target["data"], target["timestamp"])
+            count += 1
+    
+    query = {"p_hash": hashcode}
+    target = collection.find_one(query)
+    if count == 9:
+        companyname = vdoc.blocks[1].data
+        businesstype = vdoc.blocks[2].data
+        organizationsize = vdoc.blocks[3].data
+        numberofworkstations = vdoc.blocks[4].data
+        numberofservers = vdoc.blocks[5].data
+        editor = vdoc.blocks[6].data
+        typeofpolicy = vdoc.blocks[7].data
+        result = vdoc.blocks[8].data
+        hashcode = vdoc.blocks[9].p_hash
 
 
 
 print("Content-type:text/html\r\n\r\n")
+
+if gen == "Generate" and filled == 1:
+    print(score)
+elif find == "Find" and hashcode != None:
+    print(count)
 
 print("""
 <html>
@@ -152,11 +323,11 @@ print("""
 
 if companyname != None:
     print(f"""
-        <input type="text" id="companyname" name="companyname" style="width:100%;" value="{companyname}" />
+        <input type="text" id="companyname" name="companyname" style="width:100%;" value="{companyname}" required />
     """)
 else:
     print(f"""
-        <input type="text" id="companyname" name="companyname" style="width:100%;" />
+        <input type="text" id="companyname" name="companyname" style="width:100%;" required />
     """)
 
 print("""
@@ -166,7 +337,7 @@ print("""
         <td style="width:50%;">
         <label for="businesstype">Type of Business</label>
         <br />
-        <select id="businesstype" name="businesstype" style="width:150px;">
+        <select id="businesstype" name="businesstype" style="width:150px;" required>
             <option value="">-</option>
 """)
 
@@ -175,20 +346,10 @@ if businesstype == "Education":
 else:
     print("""<option value="Education">Education</option>""")
 
-if businesstype == "Retail":
-    print("""<option selected value="Retail">Retail</option>""")
-else:
-    print("""<option value="Retail">Retail</option>""")
-
 if businesstype == "IT Services":
     print("""<option selected value="IT Services">IT Services</option>""")
 else:
     print("""<option value="IT Services">IT Services</option>""")
-
-if businesstype == "Other":
-    print("""<option selected value="Other">Other</option>""")
-else:
-    print("""<option value="Other">Other</option>""")
 
 print("""
         </select>
@@ -196,7 +357,7 @@ print("""
         <td style="width:50%;">
         <label for="organizationsize">Size of Organization</label>
         <br />
-        <select id="organizationsize" name="organizationsize" style="width:150px;">
+        <select id="organizationsize" name="organizationsize" style="width:150px;" required>
             <option value="">-</option>
 """)
 
@@ -228,7 +389,7 @@ print("""
         <td>
         <label for="numberofworkstations">Number of Workstations</label>
         <br />
-        <select id="numberofworkstations" name="numberofworkstations" style="width:150px;">
+        <select id="numberofworkstations" name="numberofworkstations" style="width:150px;" required>
             <option value="">-</option>
 """)
 
@@ -258,7 +419,7 @@ print("""
         <td>
         <label for="numberofservers">Number of Servers</label>
         <br />
-        <select id="numberofservers" name="numberofservers" style="width:150px;">
+        <select id="numberofservers" name="numberofservers" style="width:150px;" required>
             <option value="">-</option>
 """)
 
@@ -297,11 +458,11 @@ print("""
 
 if editor != None:
     print(f"""
-        <input type="text" id="editor" name="editor" style="width:150px;" value="{editor}" />
+        <input type="text" id="editor" name="editor" style="width:150px;" value="{editor}" required />
     """)
 else:
     print("""
-        <input type="text" id="editor" name="editor" style="width:150px;" />
+        <input type="text" id="editor" name="editor" style="width:150px;" required />
     """)
 
 print("""
@@ -310,7 +471,7 @@ print("""
         <tr>
         <td colspan="2">
         <label for="typeofpolicy">Type of  policy to be generated: </label>
-        <select id="typeofpolicy" name="typeofpolicy" style="width:150px;">
+        <select id="typeofpolicy" name="typeofpolicy" style="width:150px;" required>
             <option value="">-</option>
 """)
 
@@ -326,7 +487,7 @@ else:
 
 print("""
         </select>
-        <input type="submit" id="gen" name="gen" value="Generate" />
+        <input type="submit" id="gen" name="gen" value="Generate" required />
         </td>
         </tr>
     </table>
@@ -342,15 +503,15 @@ print("""
 
 if hashcode != None:
     print(f"""
-        <input type="text" id="hashcode" name="hashcode" style="width:700px;" value="{hashcode}" />
+        <input type="text" id="hashcode" name="hashcode" style="width:700px;" value="{hashcode}" required />
     """)
 else:
     print(f"""
-        <input type="text" id="hashcode" name="hashcode" style="width:700px;" />
+        <input type="text" id="hashcode" name="hashcode" style="width:700px;" required />
     """)
 
 print("""
-        <input type="submit" id="find" name="find" value="Find" />
+        <input type="submit" id="find" name="find" value="Find" required />
         </table>
     </fieldset>
     </form>
@@ -361,7 +522,8 @@ print("""
         <table style="width:100%;">
 """)
 
-if gen == "Generate" and filled == 1:
+if (gen == "Generate" and filled == 1)\
+        or (find == "Find" and count == 9):
     print(f"""
         <tr>
         <td style="text-align: center; font-size: 50px">
@@ -370,7 +532,24 @@ if gen == "Generate" and filled == 1:
         </tr>
         <tr>
         <td>
-        {result}
+    """)
+    if result == "":
+        print("No rules were generated.")
+    else:
+        for i, everyrule in enumerate(result.split(".")):
+            if i < len(result.split(".")) - 1:
+                print(str(i + 1) + ". " + everyrule + ".")
+                print("<br />")
+    
+    print("""
+        </td>
+        </tr>
+    """)
+elif find == "Find" and count != 9:
+    print("""
+        <tr>
+        <td style="text-align: center; color: red; font-size: 50px">
+        Not exists
         </td>
         </tr>
     """)
@@ -389,17 +568,6 @@ print("""
 </div>
 """)
 
-#print(client.list_database_names())
-#print("<br />")
-#print(database.list_collection_names())
-#print("<br />")
-
-#for i in collection.find():
-#    print(i)
-#    print("<br />")
-
-#collection.drop()
-
 
 if gen == "Generate" and filled == 1:
     print(document.chain_size())
@@ -414,6 +582,22 @@ if gen == "Generate" and filled == 1:
         print("<br />")
         print(document.blocks[i].p_hash)
         print("<br />")
+
+
+client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+database = client["database"]
+collection = database["collection"]
+print(client.list_database_names())
+print("<br />")
+print(database.list_collection_names())
+print("<br />")
+
+for i in collection.find():
+    print(i)
+    print("<br />")
+
+#collection.drop()
+
 
 print("</body>")
 print("</html>")
---
+++ training.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords, wordnet
+from nltk import word_tokenize, pos_tag
+from nltk.stem import WordNetLemmatizer
+from pickle import dump, load
+
+class customTokenizer:
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    
+    def __call__(self, d):
+        tokenized_word = [t for t in word_tokenize(d) if t.isalnum()]
+        
+        stop_words = set(stopwords.words("english"))
+        filtered = []
+        for w in tokenized_word:
+            if w not in stop_words:
+                filtered.append(w)
+        
+        #tag = pos_tag([f])[0][1][0].upper()
+        tag_dict = {
+                "J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV
+        }
+        
+        return [self.wnl.lemmatize(f,\
+                tag_dict.get(pos_tag([f])[0][1][0].upper(), wordnet.NOUN))\
+                for f in filtered]
+
+
+txt = open("password.txt")
+line = txt.read().splitlines()
+txt.close()
+password_rule = [x for x in line if x]
+for i, x in enumerate(password_rule):
+    if x[len(x) - 1] != ".":
+        password_rule[i] += "."
+
+txt = open("wifi.txt")
+line = txt.read().splitlines()
+txt.close()
+wifi_rule = [x for x in line if x]
+for i, x in enumerate(wifi_rule):
+    if x[len(x) - 1] != ".":
+        wifi_rule[i] += "."
+
+vectorizer = TfidfVectorizer(tokenizer = customTokenizer(), norm = None)
+
+vectorizer.fit_transform(password_rule)
+dump(vectorizer, open("password.pkl", "wb"))
+
+vectorizer.fit_transform(wifi_rule)
+dump(vectorizer, open("wifi.pkl", "wb"))
+
+
+#password_pkl = load(open("password.pkl", "rb"))
+#wifi_pkl = load(open("wifi.pkl", "rb"))
+
+#v = wifi_pkl.transform(password_rule)
+
+##print(password_rule)
+##print()
+
+#print(wifi_pkl.vocabulary_)
+#print()
+
+#print(v.toarray())
+#print()
+
+#print(wifi_pkl.get_feature_names())
+
+#for i in v.toarray():
+#    for key, value in wifi_pkl.vocabulary_.items():
+#        print(key, i[value])
+
+#score = []
+#for i, j in enumerate(v.toarray()):
+#    s = 0
+#    for k, l in enumerate(wifi_pkl.get_feature_names()):
+#        if l == "wifi":
+#            j[k] *= 10
+#        if l == "company":
+#            j[k] *= 2
+#        s += j[k]
+#    score += ((wifi_rule[i], s),)
+#score.sort(key = lambda v: v[1], reverse = True)
+
+#print(score[0][1])
+#result = None
+#for m in score:
+#    if m[1] > 20:
+#        result += m[0]
+#print(result)
+
+#print(wifi_rule)
+
+#txt = open("policy_bank.txt")
+#rule = txt.read().splitlines()
+#txt.close()
+#rule2 = [x for x in rule if x]
+
+#for i, x in enumerate(rule2):
+#    if x[len(x) - 1] != ".":
+#        rule2[i] += "."
+#print(rule2)
+print("Done!")
